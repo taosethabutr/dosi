43bbdc1c0a892fbeed5078e16db3d4d6cee9ddbd
taosethabutr <taosethabutr@gmail.com>
2023-06-13 15:44:13 +0700

    Add files via upload

--- land_capture.py
+++ land_capture.py
@@ -1,8 +1,10 @@
 import time
 import os
+import asyncio
+from playwright.sync_api import sync_playwright
 from datetime import datetime, timedelta
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 # Set the dimensions of the screenshot
@@ -17,29 +19,51 @@ webhook_url = "https://discord.com/api/webhooks/1118077000846946326/S7erj7Nan8Zo
 
 # webhook_url = "https://discord.com/api/webhooks/1115884800914509864/psxWHTjiuAxNRV_ByIeWyDIB3Xfhp5oLL6xuAa_JQXvIjD2xga2KFyNK2dGIh5ByUuml"
 
+# Capture the screenshot using Playwright
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context(viewport={'width': width, 'height': height})
+    page = context.new_page()
+    page.goto(url)
+    
+    # Add CSS rule to hide the scroll bar
+    page.evaluate("document.body.style.overflow = 'hidden';")
+
+    # Wait for the page to load (increase the sleep time if needed)
+    time.sleep(5)
+    
+    # Capture the screenshot
+    screenshot_path = "screenshot.png"
+    page.screenshot(path=screenshot_path)
+    
+    # Close the browser
+    context.close()
+    browser.close()
+
+
 # Configure Chrome options for the webdriver
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode (without a GUI)
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")  # Run Chrome in headless mode (without a GUI)
 
-# Initialize the webdriver
-driver = webdriver.Chrome(options=chrome_options)
+# # Initialize the webdriver
+# driver = webdriver.Chrome(options=chrome_options)
 
-# Set the window size to the desired dimensions
-driver.set_window_size(width, height)
+# # Set the window size to the desired dimensions
+# driver.set_window_size(width, height)
 
-# Navigate to the URL
-driver.get(url)
-driver.execute_script("document.body.style.overflow = 'hidden';")
+# # Navigate to the URL
+# driver.get(url)
+# driver.execute_script("document.body.style.overflow = 'hidden';")
 
-# Wait for the page to load (increase the sleep time if needed)
-time.sleep(5)
+# # Wait for the page to load (increase the sleep time if needed)
+# time.sleep(5)
 
-# Capture the screenshot
-screenshot_path = "screenshot.png"
-driver.save_screenshot(screenshot_path)
+# # Capture the screenshot
+# screenshot_path = "screenshot.png"
+# driver.save_screenshot(screenshot_path)
 
-# Quit the webdriver
-driver.quit()
+# # Quit the webdriver
+# driver.quit()
 
 # Send the screenshot to the Discord webhook
 webhook = DiscordWebhook(url=webhook_url)
