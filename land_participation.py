de8b4a7b430b4b2cb0e72a3080a231141a62f61d
taosethabutr <taosethabutr@gmail.com>
2023-08-15 11:22:20 +0700

    Update land_participation.py

--- land_participation.py
+++ land_participation.py
@@ -256,7 +256,7 @@ for index, row in lands[['zones', 'zoneRewards', 'winnerCount', 'citizenNftCount
         line_message += row['zones']+": "+str(round(reward*0.9,4))+" - "+str(round(reward*1.1,4))+"\n"
 line_message += "\nEstimated rewards per NFT per 21,900DON (after tax):\n"
 for index, row in lands[['zones', 'zoneRewards', 'requiredDON', 'winnerCount', 'citizenNftCount', 'nftPerParticipant']].iterrows():
-    if (row['winnerCount']*row['nftPerParticipant'])/row['requiredDON']*21900*0.7,2))>0:
+    if (row['winnerCount']*row['nftPerParticipant'])/row['requiredDON']*21900*0.7>0:
         line_message += row['zones']+": "+str(round(row['zoneRewards']*0.7/(row['winnerCount']*row['nftPerParticipant'])/row['requiredDON']*21900*0.7,2))+"\n"
 print(line_notify(line_message))
 create_html_file(lands, snapshot_time, holders_lv2, holders_lv3, holders_lv4)
